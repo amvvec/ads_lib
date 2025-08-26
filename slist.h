410ed1395bc5b8d07f05489952bf090efe1db371
Akira Sato <amvvec@gmail.com>
2025-08-26 19:27:30 +0300

    Add clang-format

---
+++ slist.h
@@ -0,0 +1,17 @@
+// slist.h
+
+#ifndef SLIST_H
+#define SLIST_H
+
+typedef struct SNode SNode;
+
+SNode* createNode(int data);
+void deleteNode(SNode** head, int data);
+
+void insertFront(SNode** head, int data);
+void insertEnd(SNode** head, int data);
+
+void printList(SNode* head);
+void freeList(SNode** head);
+
+#endif // !SLIST_H
