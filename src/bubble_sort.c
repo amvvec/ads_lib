053d59666150356c919df7e78e6478ab12f30acf
Akira Sato <amvvec@gmail.com>
2025-08-28 11:42:00 +0300

    Add bubble sort algorithm

---
+++ src/bubble_sort.c
@@ -0,0 +1,17 @@
+#include "bubble_sort.h"
+
+void bubble_sort(int arr[], int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = 0; j < size - 1; j++)
+        {
+            if(arr[j] > arr[j + 1])
+            {
+                int tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
+}
--- src/main.c
+++ src/main.c
@@ -1,7 +1,30 @@
+#include "bubble_sort.h"
 #include "test_runner.h"
+
+#include <stdio.h>
 
 int main(void)
 {
     test_runner();
+
+    int arr[] = {5, 3, 8, 1, 2};
+    int size = 5;
+
+    printf("Original array: ");
+    for(int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    bubble_sort(arr, size);
+
+    printf("Sorted array: ");
+    for(int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
     return 0;
 }
