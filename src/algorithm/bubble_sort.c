5f7d4a97050bfd3ca5abd7c34f4f9d31c63c1c20
Akira Sato <amvvec@gmail.com>
2025-08-29 18:20:48 +0300

    Add overflow and injection safety for bubble sort

--- src/algorithm/bubble_sort.c
+++ src/algorithm/bubble_sort.c
@@ -1,6 +1,7 @@
 #include "bubble_sort.h"
 
 #include <stdio.h>
+#include <limits.h>
 
 /** @brief Sorts an array of integers in ascending order using bubble sort.
  *  @param arr Pointer to the array to sort.
@@ -25,6 +26,17 @@ void bubble_sort(int arr[], int size)
     {
         for(int j = 0; j < size - 1; j++)
         {
+            if ((arr + j + 1) >= (arr + size))
+            {
+                fprintf(stderr, "Buffer overflow detected at index %d\n", j);
+                return;
+            }
+            if (arr[j] > INT_MAX / 2 || arr[j + 1] > INT_MAX / 2)
+            {
+                fprintf(stderr, "Potential injection detected at index %d\n", j);
+                return;
+            }
+            
             if(arr[j] > arr[j + 1])
             {
                 int tmp = arr[j];
