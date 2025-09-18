38c2add8748aae158326bb5243eab034e84a1c07
Akira Sato <amvvec@gmail.com>
2025-09-18 23:45:10 +0300

    add graph implementation

---
+++ src/graph.c
@@ -0,0 +1,5 @@
+#include "graph.h"
+
+void init_graph()
+{
+}
---
+++ src/graph.h
@@ -0,0 +1,11 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+typedef struct
+{
+    int vertice;
+} Graph;
+
+void init_graph();
+
+#endif // !GRAPH_H
