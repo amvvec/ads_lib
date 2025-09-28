810bc781aa86549a2966b5857f4b6450c08c9a95
Akira Sato <amvvec@gmail.com>
2025-09-28 22:43:00 +0300

    add documentation to insertion sort

--- src/insertion_sort.c
+++ src/insertion_sort.c
@@ -1,10 +1,15 @@
-
-
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "insertion_sort.h"
 
+/**
+ * @brief Sorts an array using insertion sort.
+ * @param arr Array to sort.
+ * @param size Size of the array.
+ * @return 0 on success, -1 on error (null array or invalid size).
+ * @complexity O(n²) worst/average case, O(n) best case.
+ */
 int insertion_sort(int* arr, int size)
 {
     if(!arr)
@@ -12,20 +17,16 @@ int insertion_sort(int* arr, int size)
         fprintf(stderr, "Null pointer\n");
         return -1;
     }
-    
     for(int i = 0; i < size; i++)
     {
         int key = arr[i];
         int j = i - 1;
-
         while(j >= 0 && arr[j] > key)
         {
             arr[j + 1] = arr[j];
             j--;
         }
-
         arr[j + 1] = key;
     }
-
     return 0;
 }
