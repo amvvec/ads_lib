453a63397b16a256eb74a2f556b9eaf8862ef866
Akira Sato <amvvec@gmail.com>
2025-11-25 00:59:16 +0300

    add test_array_delete_nonempty

--- tests/test_array.c
+++ tests/test_array.c
@@ -46,10 +46,23 @@ static void test_array_delete_null(void)
     printf("[ PASS ] test_array_delete_null\n\n");
 }
 
+static void test_array_delete_nonempty(void)
+{
+    printf("[ RUN ] test_array_delete_nonempty\n");
+    
+    Array * a = array_new(sizeof(int));
+    assert(a != NULL);
+
+    array_delete(a);
+
+    printf("[ PASS ] test_array_delete_nonempty\n\n");
+}
+
 void run_array_tests(void)
 {
     test_array_new();
     test_array_new_zero_size();
     test_array_new_large_size();
     test_array_delete_null();
+    test_array_delete_nonempty();
 }
