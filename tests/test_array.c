4fa01726e463bbf457fffafbfce2859af35dd9ae
Akira Sato <amvvec@gmail.com>
2025-11-25 00:51:14 +0300

    add test_array_new_large_size

--- tests/test_array.c
+++ tests/test_array.c
@@ -25,8 +25,21 @@ static void test_array_new_zero_size(void)
     printf("[ PASS ] test_array_new_zero_size\n");
 }
 
+static void test_array_new_large_size(void)
+{
+    printf("\n[ RUN ] test_array_new_large_size\n");
+
+    size_t size = (((size_t)-1) / 2);
+
+    Array * array = array_new(size);
+    assert(array == NULL);
+
+    printf("[ PASS ] test_array_new_large_size\n\n");
+}
+
 void run_array_tests(void)
 {
     test_array_new();
     test_array_new_zero_size();
+    test_array_new_large_size();
 }
