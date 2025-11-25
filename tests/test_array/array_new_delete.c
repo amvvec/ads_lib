247654ae3cfc99aecf4c27b69a4e098d4164a217
Akira Sato <amvvec@gmail.com>
2025-11-25 11:15:29 +0300

    update test_new_delete

---
+++ tests/test_array/array_new_delete.c
@@ -0,0 +1,90 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../src/array.h"
+
+static void test_array_new(void)
+{
+    printf("\n[ RUN ] test_array_new\n");
+
+    Array * array = array_new(sizeof(int));
+    assert(array != NULL);
+    array_delete(array);
+
+    printf("[ PASS ] test_array_new\n");
+}
+
+static void test_array_new_zero_size(void)
+{
+    printf("\n[ RUN ] test_array_new_zero_size\n");
+
+    Array * array = array_new(0);
+    assert(array == NULL);
+
+    printf("[ PASS ] test_array_new_zero_size\n");
+}
+
+static void test_array_new_large_size(void)
+{
+    printf("\n[ RUN ] test_array_new_large_size\n");
+
+    size_t size = (((size_t)-1) / 2);
+
+    Array * array = array_new(size);
+    assert(array == NULL);
+
+    printf("[ PASS ] test_array_new_large_size\n\n");
+}
+
+static void test_array_delete_null(void)
+{
+    printf("[ RUN ] test_array_delete_null\n");
+
+    array_delete(NULL);
+
+    printf("[ PASS ] test_array_delete_null\n\n");
+}
+
+static void test_array_delete_nonempty(void)
+{
+    printf("[ RUN ] test_array_delete_nonempty\n");
+
+    Array * array = array_new(sizeof(int));
+    assert(array != NULL);
+
+    array_delete(array);
+
+    printf("[ PASS ] test_array_delete_nonempty\n\n");
+}
+
+static void test_array_free_null(void)
+{
+    printf("[ RUN ] test_array_free_null\n");
+
+    array_free(NULL);
+
+    printf("[ PASS ] test_array_free_null\n\n");
+}
+
+static void test_array_free_nonempty(void)
+{
+    printf("[ RUN ] test_array_free_nonempty\n");
+
+    Array * array = array_new(sizeof(int));
+    assert(array != NULL);
+    array_free(array);
+
+    printf("[ PASS ] test_array_free_nonempty\n\n");
+}
+
+void run_array_tests_new_delete(void)
+{
+    test_array_new();
+    test_array_new_zero_size();
+    test_array_new_large_size();
+    test_array_delete_null();
+    test_array_delete_nonempty();
+    test_array_free_null();
+    test_array_free_nonempty();
+}
