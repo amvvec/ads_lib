118126ed29e0eabeb1dad0997bc464cbe6a16f2d
Akira Sato <amvvec@gmail.com>
2025-11-30 17:09:11 +0300

    refactor test_array_init_zero_size

--- tests/test_array.c
+++ tests/test_array.c
@@ -4,6 +4,60 @@
 
 #include "../src/array.h"
 
+static void test_array_init_large_size(void)
+{
+    printf("\n[ RUN ] test_array_init_large_size\n");
+
+    size_t size = (((size_t)-1) / 2);
+
+    Array *a = array_init(size);
+
+    assert(a == NULL);
+
+    array_delete(a);
+    a = NULL;
+
+    printf("[ OK ] test_array_init_large_size\n\n");
+}
+
+static void test_array_delete_null(void)
+{
+    printf("[ RUN ] test_array_delete_null\n");
+
+    array_delete(NULL);
+
+    printf("[ OK ] test_array_delete_null\n\n");
+}
+
+static void test_array_delete_nonempty(void)
+{
+    printf("[ RUN ] test_array_delete_nonempty\n");
+
+    Array *a = array_init(sizeof a);
+
+    assert(a != NULL);
+
+    array_delete(a);
+    a = NULL;
+
+    printf("[ OK ] test_array_delete_nonempty\n");
+}
+
+static void test_array_double_delete(void)
+{
+    printf("\n[ RUN ] test_array_double_delete\n");
+
+    Array *a = array_init(sizeof(int));
+    assert(a != NULL);
+
+    array_delete(a);
+    array_delete(a);
+
+    a = NULL;
+
+    printf("[ OK ] test_array_double_delete\n");
+}
+
 static void test_array_init(void)
 {
     printf("\n[ RUN ] test_array_init\n");
@@ -17,18 +71,17 @@ static void test_array_init(void)
     printf("[ OK ] test_array_init\n");
 }
 
-static void test_array_new_zero_size(void)
+static void test_array_init_zero_size(void)
 {
-    printf("\n[ RUN ] test_array_new_zero_size\n");
+    printf("\n[ RUN ] test_array_init_zero_size\n");
 
-    Array *array = array_init(0);
+    Array * a = array_init(0);
 
-    assert(array == NULL);
+    assert(a = NULL);
+    array_delete(a);
+    a = NULL;
 
-    array_delete(array);
-    array = NULL;
-
-    printf("[ OK ] test_array_new_zero_size\n");
+    printf("[ OK ] test_array_init_zero_size\n");
 }
 
 static void test_array_new_large_size(void)
