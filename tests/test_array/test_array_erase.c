5305c197f54baafc816f46e8f75424b66c1d1976
Akira Sato <amvvec@gmail.com>
2026-01-28 06:59:21 +0300

    add test_array_erase_middle

--- tests/test_array/test_array_erase.c
+++ tests/test_array/test_array_erase.c
@@ -28,7 +28,30 @@ static void test_array_erase_back(void)
     array_delete(&a);
 }
 
+static void test_array_erase_middle(void)
+{
+    Array *a = array_init(sizeof(int));
+    assert(a != NULL);
+
+    for(int i = 0; i < 7; ++i)
+        assert(array_push_back(a, &i) == 0);
+
+    assert(array_erase(a, 3) == 0);
+    assert(array_size(a) == 6);
+
+    int expected[] = {0, 1, 2, 4, 5, 6};
+    for(size_t i = 0; i < 6; ++i)
+    {
+        int v = -1;
+        assert(array_get(a, i, &v) == 0);
+        assert(v == expected[i]);
+    }
+
+    array_delete(&a);
+}
+
 void run_array_erase_tests(void)
 {
     test_array_erase_back();
+    test_array_erase_middle();
 }
