de7ef6a3c867f75353c6d4f5ef8775cab7a02708
Akira Sato <amvvec@gmail.com>
2026-03-07 23:01:24 +0300

    test(array_insert): add test_array_insert_at_back

--- tests/test_array/test_array_insert.c
+++ tests/test_array/test_array_insert.c
@@ -36,7 +36,7 @@ test_array_insert_at_front(void)
         assert(array_insert(a, &initial[i], i) == 0);
     }
 
-    const size_t size_before = array_size(a);
+    const size_t initial_size = array_size(a);
 
     // act
     int value = 100;
@@ -48,7 +48,7 @@ test_array_insert_at_front(void)
     const int expect[] = {100,1,2,3,4,5};
     const size_t expect_n = sizeof(expect) / sizeof(expect[0]);
 
-    assert(array_size(a) == size_before + 1); // check if size increased
+    assert(array_size(a) == initial_size + 1);
 
     for(size_t i = 0; i < expect_n; ++i)
     {
@@ -74,7 +74,7 @@ test_array_insert_in_middle(void)
         assert(array_insert(a, &initial[i], i) == 0);
     }
 
-    const size_t size_before = array_size(a);
+    const size_t initial_size = array_size(a);
     
     int value = 100;
     assert(array_insert(a, &value, 3) == 0);
@@ -82,7 +82,41 @@ test_array_insert_in_middle(void)
     const int expect[] = {1,2,3,100,4,5};
     const size_t expect_n = sizeof(expect) / sizeof(expect[0]);
 
-    assert(array_size(a) == size_before + 1);
+    assert(array_size(a) == initial_size + 1);
+
+    for(size_t i = 0; i < expect_n; ++i)
+    {
+        int out;
+        assert(array_get(a, i, &out) == 0);
+        assert(out == expect[i]);
+    }
+    
+    array_delete(&a);
+}
+
+static void
+test_array_insert_at_back(void)
+{
+    Array * a = array_init(sizeof(int));
+    assert(a);
+
+    const int initial[] = {1,2,3,4,5};
+    const size_t initial_n = sizeof(initial) / sizeof(initial[0]);
+
+    for(size_t i = 0; i < initial_n; ++i)
+    {
+        assert(array_insert(a, &initial[i], i) == 0);
+    }
+
+    const size_t initial_size = array_size(a);
+
+    int value = 100;
+    assert(array_insert(a, &value, array_size(a)) == 0);
+
+    const int expect[] = {1,2,3,4,5,100};
+    const size_t expect_n = sizeof(expect) / sizeof(expect[0]);
+
+    assert(array_size(a) == initial_size + 1);
 
     for(size_t i = 0; i < expect_n; ++i)
     {
@@ -431,6 +465,7 @@ void run_array_insert_tests(void)
     test_array_insert_into_empty();
     test_array_insert_at_front();
     test_array_insert_in_middle();
+    test_array_insert_at_back();
     test_array_insert_empty_at_zero();
     test_array_insert_at_end_on_empty();
     test_array_insert_single_element_middle();
