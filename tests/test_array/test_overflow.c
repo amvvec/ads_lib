f0d07ca3e540cf61aa36f5a811ce85d587cdb154
Akira Sato <amvvec@gmail.com>
2026-04-07 18:06:16 +0300

    test: add test to safe_add

--- tests/test_array/test_overflow.c
+++ tests/test_array/test_overflow.c
@@ -13,8 +13,20 @@ test_safe_add_smoke(void)
     assert(out == 10);
 }
 
+static void
+test_safe_add_zero(void)
+{
+    size_t out;
+
+    int er = safe_add(0, 0, &out);
+
+    assert(er == 0);
+    assert(out == 0);
+}
+
 void
 run_overflow_tests(void)
 {
     test_safe_add_smoke();
+    test_safe_add_zero();
 }
