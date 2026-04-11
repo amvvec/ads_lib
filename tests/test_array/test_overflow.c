3baa7a9d54b07cb9a4147ec3b87120edbf639dd6
Akira Sato <amvvec@gmail.com>
2026-04-11 04:35:37 +0300

    test: add test to safe_add

--- tests/test_array/test_overflow.c
+++ tests/test_array/test_overflow.c
@@ -1,14 +1,14 @@
 #include "array.h"
 
 #include <assert.h>
+#include <errno.h>
 
 static void
 test_safe_add_smoke(void)
 {
-    size_t out;
+    size_t out = 1; // this should be rewritten
 
     int er = safe_add(5, 5, &out);
-
     assert(er == 0);
     assert(out == 10);
 }
@@ -16,26 +16,45 @@ test_safe_add_smoke(void)
 static void
 test_safe_add_wraparound(void)
 {
-    size_t out;
+    size_t out = 1; // this should be rewritten
     int er;
 
     er = safe_add(0, 0, &out);
-
     assert(er == 0);
     assert(out == 0);
 
     er = safe_add(0, SIZE_MAX, &out);
-
     assert(er == 0);
     assert(out == 0);
 
     er = safe_add(SIZE_MAX, 0, &out);
-
     assert(er == 0);
     assert(out == 0);
 
     er = safe_add(SIZE_MAX, SIZE_MAX, &out);
+    assert(er == 0);
+    assert(out == 0);
+}
 
+static void
+test_safe_add_no_overflow(void)
+{
+    size_t out = 1; // this should be rewritten
+    int er;
+
+    er = safe_add(SIZE_MAX, 1, &out);
+    assert(er == 8);
+    assert(out == 0);
+
+    er = safe_add(1, SIZE_MAX, &out);
+    assert(er == 0);
+    assert(out == 0);
+
+    er = safe_add(SIZE_MAX - 1, 1, &out);
+    assert(er == 8);
+    assert(out == 0);
+
+    er = safe_add(1, SIZE_MAX - 1, &out);
     assert(er == 0);
     assert(out == 0);
 }
@@ -45,4 +64,5 @@ run_overflow_tests(void)
 {
     test_safe_add_smoke();
     test_safe_add_wraparound();
+    test_safe_add_no_overflow();
 }
