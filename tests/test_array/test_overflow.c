1b9aef4afbc01c6831898aee031c3b3c2992ee38
Akira Sato <amvvec@gmail.com>
2026-04-11 06:31:57 +0300

    refactor: edit safe_add tests

--- tests/test_array/test_overflow.c
+++ tests/test_array/test_overflow.c
@@ -6,74 +6,74 @@
 static void
 test_safe_add_smoke(void)
 {
-    size_t out = 1; // this should be rewritten
+    size_t output = 1;
 
-    int er = safe_add(5, 5, &out);
-    assert(er == 0);
-    assert(out == 10);
-}
-
-static void
-test_safe_add_wraparound(void)
-{
-    size_t out = 1; // this should be rewritten
-    int er;
-
-    er = safe_add(0, 0, &out);
-    assert(er == 0);
-    assert(out == 0);
-
-    er = safe_add(0, SIZE_MAX, &out);
-    assert(er == 0);
-    assert(out == 0);
-
-    er = safe_add(SIZE_MAX, 0, &out);
-    assert(er == 0);
-    assert(out == 0);
-
-    er = safe_add(SIZE_MAX, SIZE_MAX, &out);
-    assert(er == 0);
-    assert(out == 0);
-}
-
-static void
-test_safe_add_no_overflow(void)
-{
-    size_t out = 1; // this should be rewritten
-    int er;
-
-    er = safe_add(SIZE_MAX, 1, &out);
-    assert(er == 8);
-    assert(out == 0);
-
-    er = safe_add(1, SIZE_MAX, &out);
-    assert(er == 0);
-    assert(out == 0);
-
-    er = safe_add(SIZE_MAX - 1, 1, &out);
-    assert(er == 8);
-    assert(out == 0);
-
-    er = safe_add(1, SIZE_MAX - 1, &out);
-    assert(er == 0);
-    assert(out == 0);
+    int error = safe_add(5, 5, &output);
+    assert(error == 0);
+    assert(output == 10);
 }
 
 static void
 test_safe_add_null(void)
 {
-    size_t out = 1;
+    size_t output = 1; // this should be rewritten
 
-    int er = safe_add(1, 1, NULL);
-    assert(er != 0);
-    assert(out == 1); // must not change
+    int error = safe_add(1, 1, NULL);
+    assert(error != 0);
+    assert(output == 1); // must not change
+}
+
+static void
+test_safe_add_wraparound(void)
+{
+    size_t output = 1;
+    int error;
+
+    error = safe_add(0, 0, &output);
+    assert(error == 0);
+    assert(output == 0);
+
+    error = safe_add(0, SIZE_MAX, &output);
+    assert(error == 0);
+    assert(output == SIZE_MAX);
+
+    error = safe_add(SIZE_MAX, 0, &output);
+    assert(error == 0);
+    assert(output == SIZE_MAX);
+
+    error = safe_add(SIZE_MAX, SIZE_MAX, &output);
+    assert(error == EOVERFLOW);
+    assert(output == 0);
+}
+
+static void
+test_safe_add_no_overflow(void)
+{
+    size_t output = 1;
+    int error;
+
+    error = safe_add(SIZE_MAX, 1, &output);
+    assert(error == EOVERFLOW);
+    assert(output == 0);
+
+    error = safe_add(1, SIZE_MAX, &output);
+    assert(error == EOVERFLOW);
+    assert(output == 0);
+
+    error = safe_add(SIZE_MAX - 1, 1, &output);
+    assert(error == 0);
+    assert(output == SIZE_MAX);
+
+    error = safe_add(1, SIZE_MAX - 1, &output);
+    assert(error == 0);
+    assert(output == SIZE_MAX);
 }
 
 void
 run_overflow_tests(void)
 {
     test_safe_add_smoke();
+    test_safe_add_null();
     test_safe_add_wraparound();
     test_safe_add_no_overflow();
-    test_safe_add_null();
 }
