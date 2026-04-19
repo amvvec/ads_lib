4884bdeb543e677ef0d54805375ccdc66aec07ad
Akira Sato <amvvec@gmail.com>
2026-04-19 15:43:12 +0300

    test: update add_safe tests

--- tests/test_array/test_overflow_detector.c
+++ tests/test_array/test_overflow_detector.c
@@ -8,7 +8,7 @@ test_safe_add_smoke(void)
 {
     size_t output = 1;
 
-    int error = safe_add(5, 5, &output);
+    int error = add_safe(5, 5, &output);
     assert(error == 0);
     assert(output == 10);
 }
@@ -18,7 +18,7 @@ test_safe_add_null(void)
 {
     size_t output = 1; // this should be rewritten
 
-    int error = safe_add(1, 1, NULL);
+    int error = add_safe(1, 1, NULL);
     assert(error != 0);
     assert(output == 1); // must not change
 }
@@ -29,19 +29,19 @@ test_safe_add_wraparound(void)
     size_t output = 1;
     int error;
 
-    error = safe_add(0, 0, &output);
+    error = add_safe(0, 0, &output);
     assert(error == 0);
     assert(output == 0);
 
-    error = safe_add(0, SIZE_MAX, &output);
+    error = add_safe(0, SIZE_MAX, &output);
     assert(error == 0);
     assert(output == SIZE_MAX);
 
-    error = safe_add(SIZE_MAX, 0, &output);
+    error = add_safe(SIZE_MAX, 0, &output);
     assert(error == 0);
     assert(output == SIZE_MAX);
 
-    error = safe_add(SIZE_MAX, SIZE_MAX, &output);
+    error = add_safe(SIZE_MAX, SIZE_MAX, &output);
     assert(error != 0);
     assert(output == SIZE_MAX);
 }
@@ -52,19 +52,19 @@ test_safe_add_no_overflow(void)
     size_t output = 1;
     int error;
 
-    error = safe_add(SIZE_MAX, 1, &output);
+    error = add_safe(SIZE_MAX, 1, &output);
     assert(error != 0);
     assert(output == 1);
 
-    error = safe_add(1, SIZE_MAX, &output);
+    error = add_safe(1, SIZE_MAX, &output);
     assert(error != 0);
     assert(output == 1);
 
-    error = safe_add(SIZE_MAX - 1, 1, &output);
+    error = add_safe(SIZE_MAX - 1, 1, &output);
     assert(error == 0);
     assert(output == SIZE_MAX);
 
-    error = safe_add(1, SIZE_MAX - 1, &output);
+    error = add_safe(1, SIZE_MAX - 1, &output);
     assert(error == 0);
     assert(output == SIZE_MAX);
 }
